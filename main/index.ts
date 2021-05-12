48e0f52ae41b0c0cd6d0d2982af0f2e5538f5db4
luhc228 <luhengchang228@gmail.com>
2021-05-12 11:36:07 +0800

    chore: comment

--- main/index.ts
+++ main/index.ts
@@ -4,25 +4,15 @@ import { createWindow } from './window';
 
 process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = 'true';
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow();
   handleIPC();
   app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 });
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') app.quit();
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
