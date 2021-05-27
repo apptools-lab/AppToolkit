2d5f1c0d00a9858f153c865430dfdc0ca070ffbc
luhc228 <luhengchang228@gmail.com>
2021-05-27 22:44:43 +0800

    feat: auto update

--- main/index.ts
+++ main/index.ts
@@ -2,10 +2,13 @@ import { app, BrowserWindow } from 'electron';
 import modifyProcessEnv from './utils/modifyProcessEnv';
 import { createWindow } from './window';
 import handleIPC from './ipc';
+import { checkForUpdates } from './utils/autoUpdater';
 
 process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = 'true';
 
 app.whenReady().then(() => {
+  checkForUpdates();
+
   modifyProcessEnv();
 
   createWindow();
