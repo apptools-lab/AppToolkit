efd1f3328492335ba0a45d856a921f8ebcff6eb7
Hengchang Lu <44047106+luhc228@users.noreply.github.com>
2021-08-04 14:11:05 +0800

    fix: node install and header style (#58)

--- renderer/src/components/PageHeader/index.tsx
+++ renderer/src/components/PageHeader/index.tsx
@@ -1,4 +1,5 @@
-import React, { FC } from 'React';
+import React, { FC, useEffect, useState, useRef } from 'react';
+import cn from 'classnames';
 import styles from './index.module.scss';
 
 interface IPageHeader {
@@ -6,10 +7,24 @@ interface IPageHeader {
   button?: React.ReactNode;
 }
 
-const PageHeader: FC<IPageHeader> = ({ title, button }) => {
+const PageHeader: FC<IPageHeader> = ({ title, button = null }) => {
+  const headerRef = useRef(null);
+  const [sticky, setSticky] = useState(false);
+
+  useEffect(() => {
+    function handleScroll() {
+      if (headerRef.current.offsetTop >= 40) {
+        setSticky(true);
+      } else {
+        setSticky(false);
+      }
+    }
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
   return (
-    <div className={styles.head}>
-      <span className={styles.title}>{title}</span>
+    <div className={cn(styles.header, { [styles.sticky]: sticky })} ref={headerRef}>
+      <div className={styles.title}>{title}</div>
       {button}
     </div>
   );
