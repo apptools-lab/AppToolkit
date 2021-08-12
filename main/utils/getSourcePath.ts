948a723d52e4ef99f39639b8384728514bec85c5
luhc228 <luhengchang228@126.com>
2021-08-12 11:55:15 +0800

    chore: remove log

--- main/utils/getSourcePath.ts
+++ main/utils/getSourcePath.ts
@@ -1,7 +1,6 @@
 import * as path from 'path';
 
 function getSourcePath(dir: string, ...args: string[]) {
-  console.log(process);
   const sourcePath = path.join(process.env.NODE_ENV === 'development' ? dir : process.resourcesPath, ...args);
   return sourcePath;
 }
