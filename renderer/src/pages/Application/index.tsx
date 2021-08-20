4bd60c89c632ffd23aaa4c5b5e72dc604a5a8a8f
luhc228 <luhengchang228@126.com>
2021-08-20 21:05:12 +0800

    chore: remove unnecessary apps

--- renderer/src/pages/Application/index.tsx
+++ renderer/src/pages/Application/index.tsx
@@ -3,7 +3,7 @@ import AppList from './components/AppList';
 
 function Application() {
   return (
-    <PageContainer style={{ marginBottom: 60 }} title="桌面客户端">
+    <PageContainer title="桌面客户端">
       <AppList />
     </PageContainer>
   );
