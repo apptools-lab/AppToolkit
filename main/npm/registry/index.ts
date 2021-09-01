a943496adbdd3b50f760c8ffc1722625dceed22e
Hengchang Lu <44047106+luhc228@users.noreply.github.com>
2021-09-01 10:49:39 +0800

    fix: electron not found in prod (#67)

---
+++ main/npm/registry/index.ts
@@ -0,0 +1,3 @@
+export { default as getAllRegistries } from './getAllRegistries';
+export { default as getCurrentRegistry } from './getCurrentRegistry';
+export { default as setCurrentRegistry } from './setCurrentRegistry';
