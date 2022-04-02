89f7f655b33e04c6a1468350c3c07b0790a32e2d
luhc228 <luhengchang228@126.com>
2022-04-02 21:31:28 +0800

    chore: electron version

--- packages/main/src/index.ts
+++ packages/main/src/index.ts
@@ -3,12 +3,38 @@ import { join } from 'path';
 
 const isDevelopment = import.meta.env.DEV;
 
+app.whenReady()
+  .then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+  })
+  .catch((error) => console.error('Failed to crate window:', error));
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+// app auto update
+if (!isDevelopment) {
+  app.whenReady()
+    .then(() => import('electron-updater'))
+    .then(({ autoUpdater }) => autoUpdater.checkForUpdatesAndNotify())
+    .catch((error) => console.error('Failed check updates:', error));
+}
+
 async function createWindow() {
   const mainWindow = new BrowserWindow({
     show: false, // Use 'ready-to-show' event to show window
     webPreferences: {
       preload: join(__dirname, '../../preload/build/index.cjs'),
     },
+    width: 800,
+    height: 600,
   });
 
   mainWindow.on('ready-to-show', () => {
@@ -26,21 +52,4 @@ async function createWindow() {
     : new URL(join(__dirname, '../../renderer/build/index.html'), `file://${__dirname}`).toString();
 
   await mainWindow.loadURL(pageUrl);
-}
-
-app.whenReady()
-  .then(createWindow)
-  .catch((error) => console.error('Failed to crate window:', error));
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-if (!isDevelopment) {
-  app.whenReady()
-    .then(() => import('electron-updater'))
-    .then(({ autoUpdater }) => autoUpdater.checkForUpdatesAndNotify())
-    .catch((error) => console.error('Failed check updates:', error));
 }
