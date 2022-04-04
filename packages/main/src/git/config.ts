b30547427420adc3c4bdf6d60c6a96374801ab40
luhc228 <luhengchang228@126.com>
2022-04-04 15:12:23 +0800

    feat: support user config

--- packages/main/src/git/config.ts
+++ packages/main/src/git/config.ts
@@ -1,11 +1,16 @@
+import * as path from 'path';
+import fse from 'fs-extra';
 import simpleGit from 'simple-git';
 import type { GitConfigScope } from 'simple-git';
+import { TMP_DIR } from '../constants';
+
+const userConfigDir = path.join(TMP_DIR, 'git', 'user-config');
 
 type GitConfig = Record<string, string>;
 
 export async function getGlobalConfig() {
-  const allGlobalConfig = await getConfigList({ scope: 'global' });
-  const { files: [globalConfigKey], values } = allGlobalConfig;
+  const globalConfigList = await getConfigList({ scope: 'global' });
+  const { files: [globalConfigKey], values } = globalConfigList;
 
   const globalConfig = {
     ...values[globalConfigKey],
@@ -18,7 +23,63 @@ export async function setGlobalConfig(config: GitConfig) {
   await setConfig({ config, scope: 'global' });
 }
 
-async function getConfigList({ baseDir, scope }: { baseDir?: string; scope: keyof typeof GitConfigScope }) {
+export async function addUserConfig(configId: string) {
+  const baseDir = path.join(userConfigDir, configId);
+  if (fse.pathExistsSync(baseDir)) {
+    throw new Error(`Git user config has already existed in ${baseDir}.`);
+  }
+  await fse.ensureDir(baseDir);
+  await initGit(baseDir);
+
+  return getUserConfigPath(configId);
+}
+
+export async function getUserConfig(configId: string) {
+  const baseDir = path.join(userConfigDir, configId);
+  const configList = await getConfigList({ baseDir, scope: 'local' });
+  const { files: [globalConfigKey], values } = configList;
+
+  const userConfig = {
+    ...values[globalConfigKey],
+  } as Record<string, string>;
+
+  return userConfig;
+}
+
+export async function setUserConfig(configId: string, config: GitConfig) {
+  const baseDir = path.join(userConfigDir, configId);
+  await setConfig({ scope: 'local', baseDir, config });
+}
+
+export async function removeUserConfig(configId: string) {
+  const configPath = path.join(userConfigDir, configId);
+  await fse.remove(configPath);
+}
+
+/**
+ * for example: /xxx/test/.git/config
+ */
+export function getUserConfigPath(configId: string) {
+  const configPath = path.join(userConfigDir, configId, '.git', 'config');
+  if (!fse.pathExistsSync(configPath)) {
+    throw new Error(`Git config ${configPath} is not found.`);
+  }
+  return configPath;
+}
+
+async function initGit(baseDir: string) {
+  const git = simpleGit({ baseDir });
+  await git.init();
+}
+
+async function getConfigList(
+  {
+    baseDir,
+    scope,
+  }: {
+    baseDir?: string;
+    scope: keyof typeof GitConfigScope;
+  }) {
   const git = simpleGit({ baseDir });
   return git.listConfig(scope);
 }
--- packages/main/tests/git.test.ts
+++ packages/main/tests/git.test.ts
@@ -1,5 +1,20 @@
 import { test, describe, expect } from 'vitest';
-import { getGlobalConfig, setGlobalConfig } from '../src/git';
+import { 
+  getGlobalConfig, 
+  setGlobalConfig,
+  addUserConfig,
+  getUserConfig,
+  setUserConfig,
+  removeUserConfig,
+} from '../src/git';
+import * as path from 'path';
+import * as fse from 'fs-extra';
+
+const userConfigTestId = 'test-git-user-config';
+const testGlobalConfig = { 
+  'user.name': 'test', 
+  'user.email': 'test@example.com' 
+};
 
 describe('config', () => {
   test('get global config', async () => {
@@ -9,16 +24,44 @@ describe('config', () => {
 
   test('set global config', async () => {
     const originGlobalConfig = await getGlobalConfig();
-    const testGlobalConfig = { 
-      'user.name': 'test', 
-      'user.email': 'test@example.com' 
-    }
-
     await setGlobalConfig(testGlobalConfig);
     const newGlobalConfig = await getGlobalConfig();
     expect(newGlobalConfig['user.name']).toBe(testGlobalConfig['user.name']);
     expect(newGlobalConfig['user.email']).toBe(testGlobalConfig['user.email']);
     // reset the global config
     await setGlobalConfig(originGlobalConfig);
-  })
+  });
+
+  test('add user config', async () => {
+    const userConfigPath = await addUserConfig(userConfigTestId);
+    expect(userConfigPath).not.toBeUndefined();
+  
+    await fse.remove(path.join(path.dirname(userConfigPath), '..'));
+  });
+
+  test('remove user config', async () => {
+    const userConfigPath = await addUserConfig(userConfigTestId);
+    await removeUserConfig(userConfigTestId);
+   
+    expect(await fse.pathExists(userConfigPath)).toBe(false);
+  });
+
+  test('get user config', async () => {
+    const userConfigPath = await addUserConfig(userConfigTestId);
+    const userConfig = await getUserConfig(userConfigTestId);
+    expect(userConfig).toBeDefined();
+
+    await fse.remove(path.join(path.dirname(userConfigPath), '..'));
+  });
+
+  test('set user config', async () => {
+    const userConfigPath = await addUserConfig(userConfigTestId);
+    await setUserConfig(userConfigTestId, testGlobalConfig);
+    const newGlobalConfig = await getUserConfig(userConfigTestId);
+
+    expect(newGlobalConfig['user.name']).toBe(testGlobalConfig['user.name']);
+    expect(newGlobalConfig['user.email']).toBe(testGlobalConfig['user.email']);
+    
+    await fse.remove(path.join(path.dirname(userConfigPath), '..'));
+  });
 })
