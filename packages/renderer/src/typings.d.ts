d36baef9f6270eef1d9ad8748c28be28087e0eb5
luhc228 <luhengchang228@gmail.com>
2022-05-28 20:47:40 +0800

    chore: d.ts

--- packages/renderer/src/typings.d.ts
+++ packages/renderer/src/typings.d.ts
@@ -4,21 +4,21 @@ declare global {
   interface Window {
     electronAPI: IElectronAPI;
   }
-}
 
-declare module '*.module.less' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
+  declare module '*.module.less' {
+    const classes: { [key: string]: string };
+    export default classes;
+  }
 
-declare module '*.module.css' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
+  declare module '*.module.css' {
+    const classes: { [key: string]: string };
+    export default classes;
+  }
 
-declare module '*.module.scss' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
+  declare module '*.module.scss' {
+    const classes: { [key: string]: string };
+    export default classes;
+  }
 
-declare module '*.png';
+  declare module '*.png';
+}
